9db6cced5ccd8d83a304037af7d6f8365272bb44
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-03 12:28:45 +0430

    adding some changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
-import get from "./utils/getElement.js"
-
+import get from "./utils/getElement.js";
 const URL = "https://randomuser.me/api/";
+
 const img = get(".user-img");
 const title = get(".user-title");
 const value = get(".user-values");
 const btn = get(".btn");
+const btns = [...document.querySelectorAll(".icon")]
+console.log(btns);
