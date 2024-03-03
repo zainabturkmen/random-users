b17d72c96307d92c78500fdfca606f65d05bbde6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-03 11:51:34 +0430

    adding some changes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// 
-
-const URL = "https://randomuser.me/api/";
-
-const img = document.querySelector(".user-img");
 
 function getElement(){
     const element = document.querySelector(Selection)
     if(element) return element;
     throw new Error(`no element selected`)
-}
+};
+
+const URL = "https://randomuser.me/api/";
+
+const img = document.querySelector(".user-img");
