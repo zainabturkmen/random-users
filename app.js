134405fe6cc79c5f114d2144ada058dfe311e744
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 10:51:21 +0430

    adding some changes

--- app.js
+++ app.js
@@ -9,21 +9,6 @@ const btn = get(".btn");
 const btns = [...document.querySelectorAll(".icon")];
 console.log(btns);
 
-const displayUser = (person) => {
-    img.src = person.image;
-    value.textContent = person.name;
-    title.textContent = `My name is`;
-    btns[0].classList.add("active");
-    btns.forEach((btn)=>{
-        const label = btn.dataset.label;
-        btn.addEventListener("click", ()=>{
-            title.textContent = `My ${label} is`;
-            value.textContent = person[label];
-            btns.forEach((btn) => btn.classList.remove("active"));
-            btn.classList.add("active")
-        })
-    })
-}
 
 
 const showUser = async ()=>{
---
+++ utils/displayUser.js
@@ -0,0 +1,18 @@
+const displayUser = (person) => {
+    img.src = person.image;
+    value.textContent = person.name;
+    title.textContent = `My name is`;
+    
+    btns[0].classList.add("active");
+    btns.forEach((btn)=>{
+        const label = btn.dataset.label;
+        btn.addEventListener("click", ()=>{
+            title.textContent = `My ${label} is`;
+            value.textContent = person[label];
+            btns.forEach((btn) => btn.classList.remove("active"));
+            btn.classList.add("active")
+        })
+    })
+}
+
+export default displayUser;
